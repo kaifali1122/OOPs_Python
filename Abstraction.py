e9d7518bd2dc1a160a3c3a0e9e4bfd04daee78f1
Kaif ali <140739243+kaifali1122@users.noreply.github.com>
2025-11-29 20:31:10 +0530

    Abstraction

---
+++ Abstraction.py
@@ -0,0 +1,4 @@
+# Abstractin- Hiding unnecessary detils from user through class and method
+
+# Example - here i class percentage of every one increse by 2
+#           but in object it does not reflect its wass hidden for user 
