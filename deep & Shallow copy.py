f63fd1178ac96e4ddee77ec49d7e82e7ba71093a
Kaif ali <140739243+kaifali1122@users.noreply.github.com>
2025-11-29 23:28:08 +0530

    Implement shallow and deep copy examples
    Added examples of shallow and deep copy using the copy and deepcopy functions.

--- deep & Shallow copy.py
+++ deep & Shallow copy.py
@@ -7,7 +7,19 @@
 
 
 # Need to import 
+from copy import copy , deepcopy
+
+l1=[11,2,[3,4],5]
+print("Orignal list -                   ",l1) 
 
 # Shallow copy 
+l2=copy(l1)
+l2[2].append(6)
+print("Shallow copy  list -             ",l2)
+print("Orignal list after shallow copy -",l1)
 
 # Deep copy 
+l3=deepcopy(l1)
+l3[2].append(7)
+print("Deep copy list -                 ",l3)
+print("Deep copy list after deep copy  -",l1)
