8c3c64348f46e7853c51e569ed2a3e02fd9e46d0
Kaif ali <140739243+kaifali1122@users.noreply.github.com>
2025-11-29 23:26:20 +0530

    Implement encapsulation in Student class
    Added encapsulation to the Student class to protect the percentage attribute.

--- Encapsulation.py
+++ Encapsulation.py
@@ -2,5 +2,29 @@
 # of mrthod to protect data nd enforce control access
 
 # Example- here we make percentage private using '__'
-#          now percentage is encapsulte no one can directly access it
+# now percentage is encapsulte no one can directly access it
 
+class Student: #class
+    def __init__(self, name , grade , percentage): # method 
+        self.name= name #attributes
+        self.grade = grade 
+        # using '__' we can limit percentage access
+        self.__percentage = percentage 
+        # now no one can acess percentage directly from object 
+    def get_percentage(self):
+        return self.__percentage    
+
+    def Stu_details(self):
+        print(f'{self.name} is in {self.grade}, with {self.__percentage}%') 
+
+#object
+Student1= Student('Kaif',11,94)
+Student2= Student('Atif',11,93)
+
+Student1.Stu_details() # Kaif is in 11, with 94%
+Student2.Stu_details() # Atif is in 11, with 93%
+
+# accessing private attribute using method
+print(Student1.get_percentage()) # 94
+
+print(Student1.__percentage)
