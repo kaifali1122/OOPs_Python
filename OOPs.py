e43c2ef3d7c33066efc179078ccb93d412f8e415
Kaif ali <140739243+kaifali1122@users.noreply.github.com>
2025-11-29 20:32:18 +0530

    Add comments about object-oriented programming
    Added comments explaining the concept of OOP.

---
+++ OOPs.py
@@ -0,0 +1,6 @@
+# OOPs - Object oriented programing
+
+# OOPs is a way of organizing code by creating "blue Print"(called class) 
+# to represent real world things like students, car , house
+
+# these Blueprint helps you to create object define their behaviour
